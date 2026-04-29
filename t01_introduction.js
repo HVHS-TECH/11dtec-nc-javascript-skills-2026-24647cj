203f33ca6c83e0c52a1cb29f2785b43d7315aeb9
Thunderback49 <24647cj@hvhs.school.nz>
2026-04-29 02:22:08 +0000

    Apr 29, 2026, 2:22 AM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,9 +1,21 @@
 /****************************
-This is a block comment.
-Put your header comment here!
+Name of Task:
 ****************************/
 console.log("Running t01_introduction.js")
-console.log(/*Insert your message here!*/)
+console.log("Hello World")
 
-// This is a single line comment
+// Variables
+let Username = "Chamath";
+let year = 2026;
+let age = 25;
+let money = 20;
+/****************************
+Main Code
+****************************/
+console.log("Hi"+Username+ "as of" +year+ "you are" +age+ "years old. You have" +money+ "dollars");
 
+
+
+/****************************
+Functions
+****************************/
