ca3038605eb02d51eead8ce9e68791c1e53b4739
Thunderback49 <24647cj@hvhs.school.nz>
2026-04-29 02:23:38 +0000

    Apr 29, 2026, 2:23 AM

--- t01_introduction.js
+++ t01_introduction.js
@@ -7,12 +7,12 @@ console.log("Hello World")
 // Variables
 let Username = "Chamath";
 let year = 2026;
-let age = 25;
+let age = 16;
 let money = 20;
 /****************************
 Main Code
 ****************************/
-console.log("Hi"+Username+ "as of" +year+ "you are" +age+ "years old. You have" +money+ "dollars");
+console.log("Hi "+Username+" as of " +year+ " you are " +age+ " years old. You have " +money+ " dollars.");
 
 
 
