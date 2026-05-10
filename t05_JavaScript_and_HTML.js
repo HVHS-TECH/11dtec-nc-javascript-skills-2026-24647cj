61fd41b8651bd865834c21c759f77e9bcbb920b1
Thunderback49 <24647cj@hvhs.school.nz>
2026-05-10 22:36:03 +0000

    May 10, 2026, 10:36 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -17,6 +17,8 @@ halfmoney  = money / 2;
 /****************************
 Main Code
 ****************************/
+OUTPUT.innerHTML = 
+OUTPUT.innerHTML += "<h2> Added by Javascript</h2>"
 console.log("Hi "+Username+" as of " +year+ " you are " +age+ " years old. You have " +money+ " dollars.");
 console.log("you were born in " + birthyear);
 console.log("in ten years you will be "+ oldage + " yeras old");
