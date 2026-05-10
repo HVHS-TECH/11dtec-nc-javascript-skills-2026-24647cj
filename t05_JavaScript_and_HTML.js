07a04b04c1a1fa5e214c8374005ec16dd1159986
Thunderback49 <24647cj@hvhs.school.nz>
2026-05-10 23:04:18 +0000

    May 10, 2026, 11:04 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -19,15 +19,12 @@ Main Code
 ****************************/
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
-OUTPUT.innerHTML += "<p>Hello " + userName + "</p>";
-OUTPUT.innerHTML += "<p>Second Paragraph line .< /p>";
-OUTPUT.innerHTML += "<p>Third Paragraph line .< /p>";
-console.log("Hi "+Username+" as of " +year+ " you are " +age+ " years old. You have " +money+ " dollars.");
-console.log("you were born in " + birthyear);
-console.log("in ten years you will be "+ oldage + " yeras old");
-console.log("You have " +money+ " dollars");
-console.log("you spent half your money, now you have " + halfmoney + " dollars" );
-console.log("Then you got 3 dollars, now you have " +(halfmoney+3)+" dollars");
+OUTPUT.innerHTML("Hi "+Username+" as of " +year+ " you are " +age+ " years old. You have " +money+ " dollars.");
+OUTPUT.innerHTML("you were born in " + birthyear);
+OUTPUT.innerHTML("in ten years you will be "+ oldage + " yeras old");
+OUTPUT.innerHTML("You have " +money+ " dollars");
+OUTPUT.innerHTML("you spent half your money, now you have " + halfmoney + " dollars" );
+OUTPUT.innerHTML("Then you got 3 dollars, now you have " +(halfmoney+3)+" dollars");
 
 
 
