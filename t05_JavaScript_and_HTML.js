61e09fdce5079fdde6d2083714ccb01e4d771285
Thunderback49 <24647cj@hvhs.school.nz>
2026-05-11 23:47:21 +0000

    May 11, 2026, 11:47 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -21,11 +21,10 @@ Main Code
 
 OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
 OUTPUT.innerHTML +=("Hi "+Username+" as of " +year+ " you are " +age+ " years old. You have " +money+ " dollars.");
-OUTPUT.innerHTML +=("<p>you were born in</p> " + birthyear);
-OUTPUT.innerHTML +=("in ten years you will be "+ oldage + " yeras old");
-OUTPUT.innerHTML +=("in ten years you will be "+ oldage + " yeras old");
-OUTPUT.innerHTML +=("You have " +money+ " dollars");
-OUTPUT.innerHTML +=("you spent half your money, now you have " + halfmoney + " dollars" );
+OUTPUT.innerHTML +=("<p>you were born in " + birthyear+"</p>");
+OUTPUT.innerHTML +=("<p>in ten years you will be "+ oldage + " yeras old</p>");
+OUTPUT.innerHTML +=("<p>You have " +money+ " dollars</p>");
+OUTPUT.innerHTML +=("<p>you spent half your money, now you have " + halfmoney + " dollars" );
 OUTPUT.innerHTML +=("Then you got 3 dollars, now you have " +(halfmoney+3)+" dollars");
 
 
