b47f54153a825d1a321f1acf66f71b6562af790b
Thunderback49 <24647cj@hvhs.school.nz>
2026-05-11 23:59:22 +0000

    May 11, 2026, 11:59 PM

--- t06_functions.js
+++ t06_functions.js
@@ -19,6 +19,14 @@ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 Main Code
 ****************************/
 
+writeLine()
+
+/****************************
+Functions
+****************************/
+
+function writeLine() {
+
 OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
 OUTPUT.innerHTML +=("<p>Hi "+Username+" as of " +year+ " you are " +age+ " years old. You have " +money+ " dollars.</p>s");
 OUTPUT.innerHTML +=("<p>you were born in " + birthyear+"</p>");
@@ -26,14 +34,5 @@ OUTPUT.innerHTML +=("<p>in ten years you will be "+ oldage + " yeras old</p>");
 OUTPUT.innerHTML +=("<p>You have " +money+ " dollars</p>");
 OUTPUT.innerHTML +=("<p>you spent half your money, now you have " + halfmoney + " dollars</p>" );
 OUTPUT.innerHTML +=("<p>Then you got 3 dollars, now you have " +(halfmoney+3)+" dollars</p>");
-
-
-
-/****************************
-Functions
-****************************/
-function writeLine(){
-// Add a line to the html page
-OUTPUT.innerHTML += "<p>Less code matters</p>";
 
 }
