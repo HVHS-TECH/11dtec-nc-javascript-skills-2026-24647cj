033daabb998fe884733d8a9aec5ede8ab43e7668
Thunderback49 <24647cj@hvhs.school.nz>
2026-05-27 02:36:44 +0000

    May 27, 2026, 2:36 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -25,18 +25,6 @@ writeLine()
 Functions
 ****************************/
 
-// function start() {
-
-// OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
-// OUTPUT.innerHTML +=("<p>Hi "+Username+" as of " +year+ " you are " +age+ " years old. You have " +money+ " dollars.</p>");
-// OUTPUT.innerHTML +=("<p>you were born in " + birthyear+"</p>");
-// OUTPUT.innerHTML +=("<p>in ten years you will be "+ oldage + " yeras old</p>");
-// OUTPUT.innerHTML +=("<p>You have " +money+ " dollars</p>");
-// OUTPUT.innerHTML +=("<p>you spent half your money, now you have " + halfmoney + " dollars</p>" );
-// OUTPUT.innerHTML +=("<p>Then you got 3 dollars, now you have " +(halfmoney+3)+" dollars</p>");
-
-// }
-
 function getFormInput(){
 const NAME_FIELD = document.getElementById("nameField");
 const AGE_FIELD = document.getElementById("ageField");
@@ -44,7 +32,20 @@ const COLOR_FIELD = document.getElementById("colorField");
 let userName = NAME_FIELD.value;
 let ageField = AGE_FIELD.value;
 let colorField = COLOR_FIELD.value;
-OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
-OUTPUT.innerHTML = "<p>Your age is "+age+"</p>";
-OUTPUT.innerHTML = "<p>Your favourite color is "+colorField+"</p>";
+// OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
+// OUTPUT.innerHTML = "<p>Your age is "+age+"</p>";
+// OUTPUT.innerHTML = "<p>Your favourite color is "+colorField+"</p>";
 }
+
+function start() {
+
+OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
+OUTPUT.innerHTML +=("<p>Hi "+userName+" as of " +year+ " you are " +age+ " years old. You have " +money+ " dollars.</p>");
+OUTPUT.innerHTML +=("<p>you were born in " + birthyear+"</p>");
+OUTPUT.innerHTML +=("<p>in ten years you will be "+ oldage + " yeras old</p>");
+OUTPUT.innerHTML +=("<p>You have " +money+ " dollars</p>");
+OUTPUT.innerHTML +=("<p>you spent half your money, now you have " + halfmoney + " dollars</p>" );
+OUTPUT.innerHTML +=("<p>Then you got 3 dollars, now you have " +(halfmoney+3)+" dollars</p>");
+
+}
+
