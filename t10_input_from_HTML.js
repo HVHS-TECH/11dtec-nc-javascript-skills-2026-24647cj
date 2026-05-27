2dc522f6e7991fad0db2f9fd5d5c6b9caf22f749
Thunderback49 <24647cj@hvhs.school.nz>
2026-05-27 02:30:46 +0000

    May 27, 2026, 2:30 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -25,26 +25,17 @@ writeLine()
 Functions
 ****************************/
 
-function start() {
+// function start() {
 
-OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
-OUTPUT.innerHTML +=("<p>Hi "+Username+" as of " +year+ " you are " +age+ " years old. You have " +money+ " dollars.</p>");
-OUTPUT.innerHTML +=("<p>you were born in " + birthyear+"</p>");
-OUTPUT.innerHTML +=("<p>in ten years you will be "+ oldage + " yeras old</p>");
-OUTPUT.innerHTML +=("<p>You have " +money+ " dollars</p>");
-OUTPUT.innerHTML +=("<p>you spent half your money, now you have " + halfmoney + " dollars</p>" );
-OUTPUT.innerHTML +=("<p>Then you got 3 dollars, now you have " +(halfmoney+3)+" dollars</p>");
+// OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
+// OUTPUT.innerHTML +=("<p>Hi "+Username+" as of " +year+ " you are " +age+ " years old. You have " +money+ " dollars.</p>");
+// OUTPUT.innerHTML +=("<p>you were born in " + birthyear+"</p>");
+// OUTPUT.innerHTML +=("<p>in ten years you will be "+ oldage + " yeras old</p>");
+// OUTPUT.innerHTML +=("<p>You have " +money+ " dollars</p>");
+// OUTPUT.innerHTML +=("<p>you spent half your money, now you have " + halfmoney + " dollars</p>" );
+// OUTPUT.innerHTML +=("<p>Then you got 3 dollars, now you have " +(halfmoney+3)+" dollars</p>");
 
-}
-
-
-function displayProduct(_name, _price) {
-    OUTPUT.innerHTML += "<p>" + _name + ": $" + _price + "</p>";
-}
-displayProduct("Chocolate bar", 4);
-displayProduct("Chips", 3);
-displayProduct("Drink", 2.50);
-
+// }
 
 function getFormInput(){
 const NAME_FIELD = document.getElementById("nameField");
