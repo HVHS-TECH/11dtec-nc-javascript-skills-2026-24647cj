3ef148143b270ec8bd8adf9901d21b18e2150281
Thunderback49 <24647cj@hvhs.school.nz>
2026-06-01 05:09:20 +0000

    Jun 1, 2026, 5:09 AM

--- t11_data_types.js
+++ t11_data_types.js
@@ -16,27 +16,23 @@ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
  Functions
 *********************************/
 function getFormInput() {
-    // 1. Get the DOM elements
-    const NAME_FIELD = document.getElementById("nameField");
-    const AGE_FIELD = document.getElementById("ageField");
-    const MONEY_FIELD = document.getElementById("moneyField");
+    
+const NAME_FIELD = document.getElementById("nameField");
+const AGE_FIELD = document.getElementById("ageField");
+const MONEY_FIELD = document.getElementById("moneyField");
 
-    // 2. Extract values and convert numeric strings into actual Numbers
-    let userName = NAME_FIELD.value;
-    let userAge = Number(AGE_FIELD.value);       // Crucial conversion!
-    let userMoney = Number(MONEY_FIELD.value);   // Crucial conversion!
+let userName = NAME_FIELD.value;
+let userAge = Number(AGE_FIELD.value);       
+let userMoney = Number(MONEY_FIELD.value);   
+let birthYear = year - userAge;
+let oldage = userAge + 10;
+let halfmoney = userMoney / 2;
 
-    // 3. Perform calculations using the freshly inputted user data
-    let birthYear = year - userAge;
-    let oldage = userAge + 10;
-    let halfmoney = userMoney / 2;
-
-    // 4. Update the HTML page with the new calculations
-    OUTPUT.innerHTML = "<p>Your name is " + userName + "</p>";
-    OUTPUT.innerHTML += "<p>Hi " + userName + " as of " + year + " you are " + userAge + " years old. You have " + userMoney + " dollars.</p>";
-    OUTPUT.innerHTML += "<p>You were born in " + birthYear + "</p>";
-    OUTPUT.innerHTML += "<p>In ten years you will be " + oldage + " years old</p>";
-    OUTPUT.innerHTML += "<p>You have " + userMoney + " dollars</p>";
-    OUTPUT.innerHTML += "<p>You spent half your money, now you have " + halfmoney + " dollars</p>";
-    OUTPUT.innerHTML += "<p>Then you got 3 dollars, now you have " + (halfmoney + 3) + " dollars</p>";
+OUTPUT.innerHTML = "<p>Your name is " + userName + "</p>";
+OUTPUT.innerHTML += "<p>Hi " + userName + " as of " + year + " you are " + userAge + " years old. You have " + userMoney + " dollars.</p>";
+OUTPUT.innerHTML += "<p>You were born in " + birthYear + "</p>";
+OUTPUT.innerHTML += "<p>In ten years you will be " + oldage + " years old</p>";
+OUTPUT.innerHTML += "<p>You have " + userMoney + " dollars</p>";
+OUTPUT.innerHTML += "<p>You spent half your money, now you have " + halfmoney + " dollars</p>";
+OUTPUT.innerHTML += "<p>Then you got 3 dollars, now you have " + (halfmoney + 3) + " dollars</p>";
 }
