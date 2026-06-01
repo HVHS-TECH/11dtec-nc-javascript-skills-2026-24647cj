e42b6735ab9aa63038d013d5b19b4738eef1939f
Thunderback49 <24647cj@hvhs.school.nz>
2026-06-01 05:07:21 +0000

    Jun 1, 2026, 5:07 AM

--- t11_data_types.js
+++ t11_data_types.js
@@ -1,51 +1,42 @@
-/****************************
-Name of Task: ja
-****************************/
-console.log("Running t01_introduction.js")
-console.log("Hello World")
-
-// Variables
-let Username = "Chamath";
+// Variables - Initial default values
+let username = "Chamath";
 let year = 2026;
 let age = 16;
 let money = 20;
-let birthyear;
-let  oldage;
-oldage = age+10;
-birthyear = year - age;
-halfmoney  = money / 2;
+
+// Grab the HTML output element
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-/****************************
-Main Code
-****************************/
 
-writeLine()
+/*********************************
+ Main Code
+*********************************/
+// You can leave this empty or use it to initialize default text if needed
 
-/****************************
-Functions
-****************************/
+/*********************************
+ Functions
+*********************************/
+function getFormInput() {
+    // 1. Get the DOM elements
+    const NAME_FIELD = document.getElementById("nameField");
+    const AGE_FIELD = document.getElementById("ageField");
+    const MONEY_FIELD = document.getElementById("moneyField");
 
-function getFormInput(){
+    // 2. Extract values and convert numeric strings into actual Numbers
+    let userName = NAME_FIELD.value;
+    let userAge = Number(AGE_FIELD.value);       // Crucial conversion!
+    let userMoney = Number(MONEY_FIELD.value);   // Crucial conversion!
 
-const NAME_FIELD = document.getElementById("nameField");
-const AGE_FIELD = document.getElementById("ageField");
-const BIRTHY_FIELD = document.getElementById("birthyField");
-const MONEY_FIELD = document.getElementById("moneyField");
+    // 3. Perform calculations using the freshly inputted user data
+    let birthYear = year - userAge;
+    let oldage = userAge + 10;
+    let halfmoney = userMoney / 2;
 
-let userName = NAME_FIELD.value;
-let userAge = AGE_FIELD.value;
-let birthYear = BIRTHY_FIELD.value;
-let userMoney = MONEY_FIELD.value;
-
-
-OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
-OUTPUT.innerHTML +=("<p>Hi "+userName+" as of " +year+ " you are " +userAge+ " years old. You have " +usermoney+ " dollars.</p>");
-OUTPUT.innerHTML +=("<p>you were born in " + birthYear+"</p>");
-OUTPUT.innerHTML +=("<p>in ten years you will be "+ oldage + " yeras old</p>");
-OUTPUT.innerHTML +=("<p>You have " +userMoney+ " dollars</p>");
-OUTPUT.innerHTML +=("<p>you spent half your money, now you have " + (userMoney/2) + " dollars</p>" );
-OUTPUT.innerHTML +=("<p>Then you got 3 dollars, now you have " +(halfmoney+3)+" dollars</p>");
-
+    // 4. Update the HTML page with the new calculations
+    OUTPUT.innerHTML = "<p>Your name is " + userName + "</p>";
+    OUTPUT.innerHTML += "<p>Hi " + userName + " as of " + year + " you are " + userAge + " years old. You have " + userMoney + " dollars.</p>";
+    OUTPUT.innerHTML += "<p>You were born in " + birthYear + "</p>";
+    OUTPUT.innerHTML += "<p>In ten years you will be " + oldage + " years old</p>";
+    OUTPUT.innerHTML += "<p>You have " + userMoney + " dollars</p>";
+    OUTPUT.innerHTML += "<p>You spent half your money, now you have " + halfmoney + " dollars</p>";
+    OUTPUT.innerHTML += "<p>Then you got 3 dollars, now you have " + (halfmoney + 3) + " dollars</p>";
 }
-
-
